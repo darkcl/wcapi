4ea014d33d955f341a0a27f713fb0ba88de703b1
Yeung Yiu Hung <hkclex@gmail.com>
2016-03-19 21:42:08 +0800

    + character app

---
+++ wcapi/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/darkcl/Documents/Workspace/wcapi/wcapi/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- wcapi/urls.py
+++ wcapi/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from characters.views import hello_world
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
+    url(r'^hello/$', hello_world),
     url(r'^admin/', admin.site.urls),
 ]
